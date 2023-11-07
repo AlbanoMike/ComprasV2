d0dc584dc596dc3b10dd36ef495ccca2476f4f83
Michael albano dias <49378351+AlbanoMike@users.noreply.github.com>
2023-11-06 21:22:58 -0300

    Update Main.java
    add Locale

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws ParseException {
+        @AlbanoMike's untitled project
         SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
         Scanner sc = new Scanner(System.in);
         String name;
